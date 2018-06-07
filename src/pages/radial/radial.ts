e28a9ec9b6f21ceef83b2365284df930ddcb8a33
Unknown <lucia.conde@outlook.com>
2018-06-07 09:50:12 +0200

    Radial: added 1/8 option to progr.bar + minor playback bug fix

--- src/pages/radial/radial.ts
+++ src/pages/radial/radial.ts
@@ -41,9 +41,12 @@ export class RadialPage {
     updateBar(){
       this.currentDuration=this.musicCtrl.getCurrentPlayingNotePercentage()
       /** console.log('current duration: '+ this.currentDuration); */
-      if(this.currentDuration>=0&&this.currentDuration<50){
-            this.currentNoteDuration = "1/4";
-          }
+      if(this.currentDuration>=0&&this.currentDuration<25){
+        this.currentNoteDuration = "1/8";
+      }
+      else if (this.currentDuration>=25&&this.currentDuration<50){
+        this.currentNoteDuration = "1/4";
+      }
       else if(this.currentDuration>=50&&this.currentDuration<100){
         this.currentNoteDuration = "1/2";
       }
