54eaa9409914b9c301928e50bda2bcbad3470df5
Ivar Troost <ivar@desagaz.com>
2018-06-07 22:14:42 +0200

    Add notesheet to radial piano
    Also clean up indentation

--- src/pages/radial/radial.ts
+++ src/pages/radial/radial.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MusicProvider } from "../../providers/music/music";
 
+import ABCJS from "abcjs";
+
 /**
  * Generated class for the RadialPage page.
  *
@@ -29,51 +31,58 @@ export class RadialPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RadialPage');
+    ABCJS.renderAbc("drawScore", this.musicCtrl.generateSimpleABCNotation(), {scale : 0.9, viewportHorizontal : true, scrollHorizontal : true});
   }
 
-   updateDurationProgressBar(){
+  undoNote() {
+    this.musicCtrl.undoLastNote();
+    ABCJS.renderAbc("drawScore", this.musicCtrl.generateSimpleABCNotation(), {scale : 0.9, viewportHorizontal : true, scrollHorizontal : true});
+  }
+
+  updateDurationProgressBar(){
       this.keyPressed = true;
       this.timeId = setInterval(() => {
          this.updateBar();
       }, 100);
-    }
+  }
 
-    updateBar(){
-      this.currentDuration=this.musicCtrl.getCurrentPlayingNotePercentage()
-      /** console.log('current duration: '+ this.currentDuration); */
-      if(this.currentDuration>=0&&this.currentDuration<25){
+  updateBar(){
+    this.currentDuration=this.musicCtrl.getCurrentPlayingNotePercentage()
+    /** console.log('current duration: '+ this.currentDuration); */
+    if(this.currentDuration>=0&&this.currentDuration<25){
         this.currentNoteDuration = "1/8";
-      }
-      else if (this.currentDuration>=25&&this.currentDuration<50){
+    }
+    else if (this.currentDuration>=25&&this.currentDuration<50){
         this.currentNoteDuration = "1/4";
-      }
-      else if(this.currentDuration>=50&&this.currentDuration<100){
+    }
+    else if(this.currentDuration>=50&&this.currentDuration<100){
         this.currentNoteDuration = "1/2";
-      }
-      else if(this.currentDuration>=100){
+    }
+    else if(this.currentDuration>=100){
         this.currentNoteDuration = "1";
-      }
-     }
+    }
+  }
 
-    stopProgressBar(){
+  stopProgressBar(){
       this.keyPressed = false;
       clearInterval(this.timeId);
       this.currentDuration= 0;
       this.currentNoteDuration = "0";
-    }
+  }
 
   startNotePlay(event: Event, note: string) {
-    console.log(note + " started");
-    event.stopPropagation(); // avoid double-playing for touch/mouse events
-    event.preventDefault();
-    this.musicCtrl.startNotePlay(note);
+      console.log(note + " started");
+      event.stopPropagation(); // avoid double-playing for touch/mouse events
+      event.preventDefault();
+      this.musicCtrl.startNotePlay(note);
   }
 
   stopNotePlay(event: Event) {
-    console.log("stopped note");
-    event.stopPropagation(); // avoid double-playing for touch/mouse events
-    event.preventDefault();
-    this.musicCtrl.stopNotePlay()
+      console.log("stopped note");
+      event.stopPropagation(); // avoid double-playing for touch/mouse events
+      event.preventDefault();
+      this.musicCtrl.stopNotePlay();
+      ABCJS.renderAbc("drawScore", this.musicCtrl.generateSimpleABCNotation(), {scale : 0.9, viewportHorizontal : true, scrollHorizontal : true});
   }
 
 }
